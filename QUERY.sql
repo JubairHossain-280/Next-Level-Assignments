ec423719e559e125c8f05e066bccd1fb108e13c8
JubairHossain-280 <jubaircse280@gmail.com>
2026-07-08 15:37:44 +0600

    Implementation of queries 3 and 4

--- QUERY.sql
+++ QUERY.sql
@@ -121,3 +121,29 @@ FROM
   users
 WHERE
   full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+-- =========================================================================
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+-- =========================================================================
+SELECT
+  booking_id,
+  user_id,
+  match_id,
+  COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM
+  bookings
+WHERE
+  payment_status IS NULL;
+
+-- =========================================================================
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+-- =========================================================================
+SELECT
+  booking_id,
+  full_name,
+  fixture,
+  total_cost
+FROM
+  bookings
+  JOIN users ON bookings.user_id = users.user_id
+  JOIN matches ON bookings.match_id = matches.match_id;
